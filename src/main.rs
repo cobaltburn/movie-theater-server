1749caaaaf2066d8617986e31f9e1c7becabefa9
michael <michaelpederson85@gmail.com>
2023-10-31 21:09:32 -0500

    began purchase page added modified database
    moved seats to table and linked them to showtime
    added account, and purchase table

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use askama::Template;
+use askama_axum::IntoResponse;
 use axum::{
-    extract::Path,
+    extract::{Form, Path},
     http::StatusCode,
-    response::Result,
-    routing::{get, get_service},
+    response::{Redirect, Response, Result},
+    routing::{get, get_service, post},
     Router,
 };
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
 use surrealdb::{
     engine::remote::ws::{Client, Ws},
     opt::auth::Root,
@@ -31,6 +31,10 @@ struct Temp {}
 #[derive(Template)]
 #[template(path = "index.html")]
 struct Index {}
+
+#[derive(Template)]
+#[template(path = "unavailable.html")]
+struct Unavailable {}
 
 #[derive(Template)]
 #[template(path = "home.html")]
@@ -73,6 +77,13 @@ struct ConfirmationPage {
     seat: i32,
     movie: Movie,
 }
+#[derive(Template)]
+#[template(path = "complete.html")]
+struct Complete {
+    movie: String,
+    time: String,
+    seat: i32,
+}
 
 #[derive(Template)]
 #[template(path = "purchase.html")]
@@ -81,12 +92,34 @@ struct PurchasePage {
     time: String,
     seat: i32,
     movie: String,
+    card_num: String,
+    exp_date: String,
+    cvv: String,
+    email: String,
+    valid_email: bool,
+}
+
+impl PurchasePage {
+    fn new(id: String, time: String, seat: i32, movie: String) -> PurchasePage {
+        PurchasePage {
+            id,
+            time,
+            seat,
+            movie,
+            card_num: String::new(),
+            exp_date: String::new(),
+            cvv: String::new(),
+            email: String::new(),
+            valid_email: true,
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Seat {
     available: bool,
-    seat: i32,
+    number: i32,
+    id: Thing,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -97,32 +130,47 @@ struct Record {
 #[derive(Debug, Serialize, Deserialize)]
 struct Movie {
     id: Thing,
-    name: Arc<str>,
-    genres: Vec<Arc<str>>,
+    name: String,
+    genres: Vec<String>,
     runtime: i32,
-    tagline: Arc<str>,
+    tagline: String,
     stars: f32,
-    description: Arc<str>,
-    image: Arc<str>,
+    description: String,
+    image: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MovieShowTimes {
     id: Thing,
-    name: Arc<str>,
-    genres: Vec<Arc<str>>,
+    name: String,
+    genres: Vec<String>,
     runtime: i32,
-    tagline: Arc<str>,
+    tagline: String,
     stars: f32,
-    description: Arc<str>,
-    image: Arc<str>,
+    description: String,
+    image: String,
     times: Vec<Time>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Time {
     id: Thing,
-    time: Arc<str>,
+    time: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct UserInfo {
+    card_num: String,
+    exp_date: String,
+    cvv: String,
+    email: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ShowtimeSeat {
+    id: Thing,
+    seat: i32,
+    time: String,
 }
 
 static DB: Lazy<Surreal<Client>> = Lazy::new(Surreal::init);
@@ -143,7 +191,10 @@ async fn main() -> anyhow::Result<()> {
         .route("/booking/:id", get(booking))
         .route("/seating/:id", get(seating))
         .route("/seating/:id/:seat", get(select_seat))
+        .route("/purchase/unavailable", get(unavailable))
         .route("/purchase/:id/:seat", get(purchase))
+        .route("/purchase/:id/:seat/:movie/:time", post(complete_purchase))
+        .route("/purchase/complete/:seat/:movie/:time", get(complete))
         .nest_service("/images", get_service(ServeDir::new("images")));
 
     println!("Listening on http://{ADDR}");
@@ -243,15 +294,16 @@ async fn seating(Path(id): Path<String>) -> Result<SeatingPage> {
     let (_, showtime_id) = split_id;
 
     let query = DB
-        .query(r#"SELECT VALUE seats FROM type::thing("showtime",$id)"#)
+        .query(r#"SELECT VALUE ->showtime_seat->seats.* FROM ONLY type::thing("showtime",$id)"#)
         .bind(("id", showtime_id))
         .await;
     let Ok(mut query) = query else {
         return Err(StatusCode::NOT_ACCEPTABLE.into());
     };
-    let Some(seats) = query.take(0).expect("invalid-query index") else {
+    let Ok(mut seats): Result<Vec<Seat>, _> = query.take(0) else {
         return Err(StatusCode::NOT_ACCEPTABLE.into());
     };
+    seats.sort_by(|a, b| a.number.cmp(&b.number));
 
     Ok(SeatingPage { id, seats })
 }
@@ -286,12 +338,76 @@ async fn purchase(Path((id, seat)): Path<(String, i32)>) -> Result<PurchasePage>
         return Err(StatusCode::NOT_ACCEPTABLE.into());
     };
 
-    Ok(PurchasePage {
-        id,
-        time,
-        seat,
-        movie,
-    })
+    Ok(PurchasePage::new(id, time, seat, movie))
+}
+
+async fn complete_purchase(
+    Path((id, seat, movie, time)): Path<(String, i32, String, String)>,
+    email: Option<Form<UserInfo>>,
+) -> Response {
+    let Some(Form(email)) = email else {
+        println!("invalid query");
+        println!("{:?}", email);
+        return StatusCode::NOT_ACCEPTABLE.into_response();
+    };
+    let UserInfo {
+        email,
+        card_num,
+        exp_date,
+        cvv,
+    } = email;
+    if !valid_email(&email) {
+        return PurchasePage {
+            id,
+            time,
+            seat,
+            movie,
+            card_num,
+            exp_date,
+            cvv,
+            email,
+            valid_email: false,
+        }
+        .into_response();
+    }
+
+    let Some(split_id) = id.split_once(':') else {
+        return StatusCode::NOT_ACCEPTABLE.into_response();
+    };
+    let (_, showtime_id) = split_id;
+    let query = DB
+        .query(
+            r#"
+            SELECT id, seats[WHERE seat = $seat AND available = true].seat AS seat, time::format(time, "%k:%M, %e %h") AS time
+            FROM type::thing("showtime",$id) 
+            SPLIT seat
+            "#,
+        )
+        .bind(("seat", seat))
+        .bind(("id", showtime_id))
+        .await;
+
+    let Ok(mut query) = query else {
+        return StatusCode::NOT_ACCEPTABLE.into_response();
+    };
+
+    let Some(showtime_seat): Option<ShowtimeSeat> = query.take(0).expect("invalid query index")
+    else {
+        return Redirect::to("/purchase/unavailable").into_response();
+    };
+
+    //TODO valid that seat is available
+    //TODO return hash value for purchase
+    let url = format!("/purchase/complete/{}/{}/{}", seat, movie, time);
+    Redirect::to((*url).into()).into_response()
+}
+
+async fn complete(Path((seat, movie, time)): Path<(i32, String, String)>) -> Complete {
+    Complete { movie, time, seat }
+}
+
+async fn unavailable() -> Unavailable {
+    Unavailable {}
 }
 
 async fn booking(Path(id): Path<String>) -> BookingPage {
@@ -300,4 +416,12 @@ async fn booking(Path(id): Path<String>) -> BookingPage {
 
 async fn movie(Path(_): Path<String>) -> Temp {
     Temp {}
+}
+
+fn valid_email(email: &String) -> bool {
+    let email_pattern =
+        regex::Regex::new(r#"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#).unwrap();
+
+    // Check if the provided email matches the pattern.
+    email_pattern.is_match(email)
 }
